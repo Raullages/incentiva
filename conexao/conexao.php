0c958cee7a60bbeb29de77720b94f17cb6898628
Raullages <raul.lages@hotmail.com>
2017-09-05 15:33:15 -0300

    Update conexao.php

--- conexao/conexao.php
+++ conexao/conexao.php
@@ -3,8 +3,10 @@ $servidor = 'incentiva.database.windows.net';
 $usuario = 'raullages31';
 $senha = 'Eminem31';
 $banco = 'incentiva construtora';
-    
-$conexao = mssql_connect($servidor,$usuario,$senha,$banco);
+
+$connectio_info = array("UID"=>$usuario, "PWD"=>$senha, "Database"=>$banco);
+
+$conexao = sqlsrv_connect($servidor,$connectio_info);
 $selecao = mssql_select_db($conexao,$banco);
 
 if(!$conexao){
