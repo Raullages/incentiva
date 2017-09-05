9037ee54942a25444c5f2e9a17451f9682ef5188
krilmun <lucas.antunes_@outlook.com>
2017-09-05 17:14:55 -0300

    incentiva

--- conexao/conexao.php
+++ conexao/conexao.php
@@ -1,5 +1,5 @@
 <?php 
-$servidor = 'incentiva.database.windows.net';
+$servidor = 'tcp:incentiva.database.windows.net,1433';
 $usuario = 'raullages31';
 $senha = 'Eminem31';
 $banco = 'incentiva construtora';
@@ -11,4 +11,19 @@ if(!$conexao){
     echo "Erro ao Conectar com o Banco";
 }
 
+// PHP Data Objects(PDO) Sample Code:
+/*try {
+    $conexao = new PDO("sqlsrv:server = tcp:incentiva.database.windows.net,1433; Database = incentiva construtora", "raullages31", "Eminem31");
+    $conexao->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+// SQL Server Extension Sample Code:
+$selecao = array("UID" => "raullages31@incentiva", "pwd" => "{your_password_here}", "Database" => "incentiva construtora", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$servidor = "tcp:incentiva.database.windows.net,1433";
+$conexao = sqlsrv_connect($serverName, $connectionInfo);*/
+	
 ?>
