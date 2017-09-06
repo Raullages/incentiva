808399845f869e991b98c9f30278d986d43cae46
Raullages <raul.lages@hotmail.com>
2017-09-05 21:35:35 -0300

    incentiva

--- conexao/conexao.php
+++ conexao/conexao.php
@@ -1,15 +1,17 @@
 <?php 
-$servidor = 'tcp:incentiva.database.windows.net,1433';
+$servidor = 'incentiva.database.windows.net';
 $usuario = 'raullages31';
 $senha = 'Eminem31';
 $banco = 'incentiva construtora';
     
-$conexao = mysqli_connect($servidor,$usuario,$senha,$banco);
-$selecao = mysqli_select_db($conexao,$banco);
+$conexao = mysql_connect($servidor,$usuario,$senha,$banco);
+$selecao = mysql_select_db($conexao,$banco);
 
 
 if(!$conexao){
-    echo "Erro ao Conectar com o Banco";
+    echo "Erro ao Conectar com o Banco Teste";
+}else{
+	echo 'Conexao efetuada';
 }
 
 ?>
